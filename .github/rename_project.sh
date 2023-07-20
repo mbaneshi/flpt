654749e32450252591791e51920ed6f6a72ec58e
mbaneshi <mbaneshi@users.noreply.github.com>
2023-07-20 22:36:01 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="mbaneshi"
+original_name="flpt"
+original_urlname="flpt"
+original_description="Awesome flpt created by mbaneshi"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv flpt $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- flpt/ext/commands.py
+++ flpt/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from flpt.ext.auth import create_user
+from flpt.ext.database import db
+from flpt.models import Product
 
 
 def create_db():
